5c9cac37b165d02147070b655ee750d4bf024288
“dalime” <Danny.B.Lim@gmail.com>
2024-08-18 13:50:51 +0700

    Fixed responsive error

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
       </div>
 
       <div
-        className={`mb-10 grid text-center lg:mb-0 lg:w-full lg:grid-cols-2 lg:text-center mt-4 gap-5${isMobile ? ' mt-32' : ''}`}
+        className={`mb-10 grid text-center lg:mb-0 lg:w-full lg:grid-cols-2 lg:text-center gap-5${isMobile ? ' mt-32' : 'mt-4'}`}
       >
         <CandidateCard
           candidate="Trump"
